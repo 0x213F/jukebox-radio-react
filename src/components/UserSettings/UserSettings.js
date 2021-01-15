00441a6e8e7bf4b2cebade831d875b93fb938b3a
op1346 <olivia.park1346@gmail.com>
2021-01-15 17:08:49 +0900

    Add Autheticate Spotify to UserSettings

--- src/components/UserSettings/UserSettings.js
+++ src/components/UserSettings/UserSettings.js
@@ -1,10 +1,25 @@
-import React from "react";
-import styles from './UserSettings.module.css';
+import React, { useState } from "react";
+import { fetchBackend } from '../../utils/network'
 
 function UserSettings() {
-  return (
 
-  );
+  const authenticateSpotify = async() => {
+    await fetchBackend("http://localhost:8000/users/user-connect-spotify")
+    .then((response) => response.json())
+    .then((data) =>{
+      this.setState({spotifyAuthenticated: data.status });
+      if (!data.status) {
+        fetchBackend("http://localhost:8000/users/user-connect-spotify")
+        .then((response) =>  response.json())
+        .then((data) => {
+          window.location = data.url;
+        });
+      }
+    });
+  }
+    return (
+      <button type="submit" onClick={authenticateSpotify}>Connect Your Spotify Account</button>
+    );
 }
 
 export default UserSettings;
