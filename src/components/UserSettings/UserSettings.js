2701c4070e1ebdd4b6b1384783d73cfc1a3d67b2
op1346 <olivia.park1346@gmail.com>
2021-02-13 13:14:56 +0900

    Update UserSettings to add idle, voice, and focus modes

--- src/components/UserSettings/UserSettings.js
+++ src/components/UserSettings/UserSettings.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from 'react-redux';
 
 function UserSettings(props) {
+  const [ idleQueue, setIdleQueue] = useState(false);
+  const [ speakVoice, setSpeakVoice] = useState(true);
+  const [ focusMode, setFocusMode] = useState(false);
+
   if (!props.userSettings) {
     return <></>;
   }
 
   return (
-    <a href={props.userSettings.spotify.authorizationUrl}>Connect Your Spotify Account</a>
+    <div>
+      <a href={props.userSettings.spotify.authorizationUrl}>Connect Your Spotify Account</a>
+
+      <form>
+        <label>
+          <input type="checkbox" name="idleQueue" checked={idleQueue} onChange={() => setIdleQueue(!idleQueue)}/>
+          Idle After queue
+        </label><br/>
+        <label>
+          <input type="checkbox" name="speakVoiceRecordings" checked={speakVoice} onChange={() => setSpeakVoice(!speakVoice)}/>
+          Speak Voice Recordings
+        </label><br/>
+        <label>
+          <input type="checkbox" name="focusMode" checked={focusMode} onChange={() => setFocusMode(!focusMode)}/>
+          Focus Mode
+        </label>
+      </form>
+    </div>
   );
 }
 
