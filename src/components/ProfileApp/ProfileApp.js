f610c338e7e2259a380962ac361436b21a9d9ede
op1346 <olivia.park1346@gmail.com>
2021-02-23 20:48:00 +0900

    Update ProfileApp.js

--- src/components/ProfileApp/ProfileApp.js
+++ src/components/ProfileApp/ProfileApp.js
@@ -7,37 +7,38 @@ function ProfileApp(props) {
   // }
 
   const username = 'Olivia Park';
-  const profileImg = 'https://flic.kr/p/2kDxxZi';
+  const profileImg = 'https://images.unsplash.com/photo-1614038276039-667c23bc32fa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=3000&q=80';
   const description = 'The bomb';
   const website = 'https://www.oliviahpark.com';
   const sessionList = [
     {
-      'image': "https://flic.kr/p/2kDMzbQ",
+      'image': "https://images.unsplash.com/photo-1614028609503-590a6a47146a?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
       'title': "My Session",
-      'duration': 252000,
+      'duration': "252000ms",
     },
     {
-      'image': "https://flic.kr/p/2kDGC7o",
+      'image': "https://images.unsplash.com/photo-1614070501149-ce4d75b91ada?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
       'title': "My Session2",
-      'duration': 222000,
+      'duration': "222000",
     },
   ]
 
   return (
     <div>
       <h1>{username}</h1>
-      <img src={profileImg} alt="profileImg"></img>
+      <img src={profileImg} alt="profileImg" style={{width:300, height:400}}/>'
+      <h2>Who is {username}?</h2>
       <p>{description}</p>
-      <a href={website} target='_blank' rel='noreferrer'>{username} Website</a>
+      <a href={website} target='_blank' rel='noreferrer'>{username}'s Website</a>
       <h2>Sessions</h2>
-      {!sessionList ? <h2>This user has no sessions.</h2>
+      {sessionList.length === 0 ? <h3>{username} has no sessions.</h3>
       :
       sessionList.map(session =>(
         <div>
           <ul>
             <li>
               {session.title}<br/>
-              {session.image}<br/>
+              <img src={session.image} alt="sessionImage" style={{width:300, height:400}} /><br/>
               {session.duration}
             </li>
           </ul>
