0cf0db8c3b214c5cca939503a90869d1ccd91cd1
op1346 <olivia.park1346@gmail.com>
2021-02-24 10:05:14 +0900

    Update UserSettings.js

--- src/components/UserSettings/UserSettings.js
+++ src/components/UserSettings/UserSettings.js
@@ -7,10 +7,19 @@ function UserSettings(props) {
 
   // TODO actually impliment the disabled behavior
   // eslint-disable-next-line
-  const [controlsEnabled, setControlsEnabled] = useState(true);
+  const [idleControls, setIdleControls] = useState(true);
+  const [voiceControls, setVoiceControls] = useState(true);
+  const [focusControls, setFocusControls] = useState(true);
 
   if (!props.userSettings) {
     return <></>;
+  }
+
+  function updateUserSettings(event) {
+    props.dispatch({type: 'userSettings/update', payload:{
+      event.target.checked
+    }});
+    fetchUpdateUserSettings(, event.target.checked);
   }
 
   function updateIdleQueue(event) {
