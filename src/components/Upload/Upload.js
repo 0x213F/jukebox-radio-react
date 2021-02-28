78cc82beb9573069d4c9bae1de5dc17d740b3486
Josh <josh@schultheiss.io>
2021-02-28 14:41:15 -0800

    Fix upload and search form

--- src/components/Upload/Upload.js
+++ src/components/Upload/Upload.js
@@ -10,6 +10,9 @@ function Upload(props) {
   /*
    * 🏗
    */
+  const [inputKey, setInputKey] = useState(0);
+
+  const [isDisabled, setIsDisabled] = useState(false);
   const [audioFile, setAudioFile] = useState('');
   const [imageFile, setImageFile] = useState('');
   const [trackName, setTrackName] = useState('');
@@ -22,8 +25,10 @@ function Upload(props) {
   /*
    * When the user initializes a login attempt.
    */
-  const handleSubmit = async function(e) {
-    e.preventDefault();
+  const handleSubmit = async function() {
+
+    // Hit the server.
+    setIsDisabled(true);
     await fetchCreateTrack(
       audioFile,
       imageFile,
@@ -31,32 +36,43 @@ function Upload(props) {
       artistName,
       albumName,
     );
+
+    // Reset the form.
+    setIsDisabled(false);
+    setInputKey(prev => prev + 2);
     setTrackName('');
     setArtistName('');
     setAlbumName('');
+
+    // Close the modal.
     closeModal();
   }
 
   return (
     <Modal isOpen={isOpen}
            ariaHideApp={false}>
-      <button onClick={closeModal}>Close</button>
+      <button onClick={closeModal}
+              disabled={isDisabled}>Close</button>
 
-      <form className={styles.Login} onSubmit={async (e) => { await handleSubmit(e); }}>
+      <div className={styles.Login}>
         <h3>Upload</h3>
 
         <label className={styles.FormBlock}>
           Audio file
           <input type="file"
                  name="audioFile"
-                 onChange={(e) => {setAudioFile(e.target.files[0])}} />
+                 onChange={(e) => {setAudioFile(e.target.files[0])}}
+                 disabled={isDisabled}
+                 key={inputKey} />
         </label>
 
         <label className={styles.FormBlock}>
           Image file
           <input type="file"
                  name="imageFile"
-                 onChange={(e) => {setImageFile(e.target.files[0])}} />
+                 onChange={(e) => {setImageFile(e.target.files[0])}}
+                 disabled={isDisabled}
+                 key={inputKey + 1} />
         </label>
 
         <br></br>
@@ -67,7 +83,8 @@ function Upload(props) {
                  name="trackName"
                  placeholder="Track name"
                  value={trackName}
-                 onChange={(e) => {setTrackName(e.target.value)}} />
+                 onChange={(e) => {setTrackName(e.target.value)}}
+                 disabled={isDisabled} />
         </label>
 
         <label className={styles.FormBlock}>
@@ -76,7 +93,8 @@ function Upload(props) {
                  name="artistName"
                  placeholder="Artist name"
                  value={artistName}
-                 onChange={(e) => {setArtistName(e.target.value)}} />
+                 onChange={(e) => {setArtistName(e.target.value)}}
+                 disabled={isDisabled} />
         </label>
 
         <label className={styles.FormBlock}>
@@ -85,17 +103,20 @@ function Upload(props) {
                  name="albumName"
                  placeholder="Album name"
                  value={albumName}
-                 onChange={(e) => {setAlbumName(e.target.value)}} />
+                 onChange={(e) => {setAlbumName(e.target.value)}}
+                 disabled={isDisabled} />
         </label>
 
         <br></br>
 
         <div className={styles.FormBlock}>
-          <button type="submit">
+          <button type="submit"
+                  onClick={handleSubmit}
+                  disabled={isDisabled}>
             Submit
           </button>
         </div>
-      </form>
+      </div>
     </Modal>
   );
 }
