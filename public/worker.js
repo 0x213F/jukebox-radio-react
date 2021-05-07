de159d8c2e51d3af2b92df656dbe090af6dca53c
Josh <josh@schultheiss.io>
2021-05-07 03:13:54 -0700

    Fix web worker

--- public/worker.js
+++ public/worker.js
@@ -1,10 +1,14 @@
 self.addEventListener('fetch', function(event) {
   event.respondWith(async function() {
-    return {
-      system: {
-        status: 400,
-        message: "Offline",
-      },
-    };
+    try {
+      return await fetch(event.request);
+    } catch(error) {
+      return {
+        system: {
+          status: 400,
+          message: "Offline",
+        },
+      };
+    }
   }());
 });
