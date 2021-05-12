3ff438d396fb0cfb8c5c60be99bd000eebd457fe
Josh <josh@schultheiss.io>
2021-05-11 23:14:53 -0700

    Add logos to search results

--- src/components/SearchApp/SearchResult/SearchResult.js
+++ src/components/SearchApp/SearchResult/SearchResult.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import styles from './SearchResult.module.css';
 // import { iconCheck } from './icons';
-import { SERVICE_APPLE_MUSIC } from './../../../config/services';
+import { SERVICE_SPOTIFY, SERVICE_YOUTUBE, SERVICE_APPLE_MUSIC, SERVICE_JUKEBOX_RADIO, SERVICE_AUDIUS } from '../../../config/services';
+import { iconSpotify, iconYouTube, iconAppleMusic, iconLogoAlt, iconAudius } from '../../../icons';
 
 
 function SearchResult(props) {
@@ -18,9 +19,23 @@ function SearchResult(props) {
   }
 
   let imgUrl = searchResult.img_url;
+  console.log(searchResult)
   if(searchResult.provider === SERVICE_APPLE_MUSIC) {
     imgUrl = imgUrl.replace("{w}", "300");
     imgUrl = imgUrl.replace("{h}", "300");
+  }
+
+  let serviceSvg;
+  if(searchResult.provider === SERVICE_SPOTIFY) {
+    serviceSvg = iconSpotify;
+  } else if(searchResult.provider === SERVICE_YOUTUBE) {
+    serviceSvg = iconYouTube;
+  } else if(searchResult.provider === SERVICE_APPLE_MUSIC) {
+    serviceSvg = iconAppleMusic;
+  } else if(searchResult.provider === SERVICE_JUKEBOX_RADIO) {
+    serviceSvg = iconLogoAlt;
+  } else if(searchResult.provider === SERVICE_AUDIUS) {
+    serviceSvg = iconAudius;
   }
 
   return (
@@ -33,6 +48,10 @@ function SearchResult(props) {
       <div className={styles.ResultInformation}>
         <h5>{searchResult.name}</h5>
         <h6>{searchResult.artist_name}</h6>
+      </div>
+
+      <div className={styles.ServiceLogo}>
+        {serviceSvg}
       </div>
 
       { /* Initial state: button has not been clicked. */
